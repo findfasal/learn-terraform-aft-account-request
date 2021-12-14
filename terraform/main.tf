1e81794d6c7abb6377b6d923dee3ccc531f015de
findfasal <findfasal@gmail.com>
2021-12-14 17:19:17 +0000

    Create new Prod account for Business-B under Business-B-OU

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,30 @@ module "sandbox" {
 
   account_customizations_name = "BusinessA"
 }
+module "BusinessB" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "cloudncloud6@gmail.com"
+    AccountName               = "Business-B-Prod"
+    ManagedOrganizationalUnit = "BusinessB-OU"
+    SSOUserEmail              = "cloudncloud4@gmail.com"
+    SSOUserFirstName          = "Faslu"
+    SSOUserLastName           = "Rahman"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "Business-B"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "nprod"
+  }
+
+  account_customizations_name = "BusinessB-Prod"
+}
